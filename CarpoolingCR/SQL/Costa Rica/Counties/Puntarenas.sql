5186d173081b5d4ed935c42e65331f371af17d24
Pablo Villalobos <pavr1@hotmail.com>
2019-08-06 17:36:44 -0700

    Locations sql

---
+++ CarpoolingCR/SQL/Costa Rica/Counties/Puntarenas.sql
@@ -0,0 +1,16 @@
+﻿declare @ProvinceName varchar(50)
+declare @ProvinceId int
+
+set @ProvinceName = 'Puntarenas'
+
+select @ProvinceId = ProvinceId from provinces where name = @ProvinceName
+
+INSERT INTO [dbo].[Counties] ([Name],[ProvinceId]) VALUES ('Puntarenas',@ProvinceId)
+INSERT INTO [dbo].[Counties] ([Name],[ProvinceId]) VALUES ('Esparza',@ProvinceId)
+INSERT INTO [dbo].[Counties] ([Name],[ProvinceId]) VALUES ('Buenos Aires',@ProvinceId)
+INSERT INTO [dbo].[Counties] ([Name],[ProvinceId]) VALUES ('Osa',@ProvinceId)
+INSERT INTO [dbo].[Counties] ([Name],[ProvinceId]) VALUES ('Quepos',@ProvinceId)
+INSERT INTO [dbo].[Counties] ([Name],[ProvinceId]) VALUES ('Coto Brus',@ProvinceId)
+INSERT INTO [dbo].[Counties] ([Name],[ProvinceId]) VALUES ('Parrita',@ProvinceId)
+INSERT INTO [dbo].[Counties] ([Name],[ProvinceId]) VALUES ('Corredores',@ProvinceId)
+INSERT INTO [dbo].[Counties] ([Name],[ProvinceId]) VALUES ('Garabito',@ProvinceId)
